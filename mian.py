d7632f07e6e1084786cf8ce896c78a0af928f77d
imldy <im.ldy@outlook.com>
2020-11-20 14:05:16 +0800

    将保存的文件名“年-月-日-原文件名”改为“年-月-日-图片ID+后缀名”

--- mian.py
+++ mian.py
@@ -30,15 +30,16 @@ class Blogger(object):
 
 
 class Picture(object):
-    def __init__(self, createdTime, picName, picTimeStamp, picHost):
+    def __init__(self, photoID, picName, picTimeStamp, picHost):
         # # 如果含"日"，则代表要修改日期显示的格式
         # if "日" in self.createdTime:
         #     self.createdTime = "{}-{}".format("", self.createdTime.replace("日", "").replace("月", "-"))
+        self.picID = photoID
         self.picName = picName
         self.picHost = picHost
         self.picTimeStamp = picTimeStamp
         self.createdTime = time.strftime("%Y-%m-%d", time.localtime(self.picTimeStamp))
-        self.picEntireName = "{}-{}".format(self.createdTime, self.picName)
+        self.picEntireName = "{}-{}{}".format(self.createdTime, self.picID, os.path.splitext(self.picName)[-1])
         self.path = ""
 
 
@@ -84,7 +85,7 @@ class User(object):
             # print(type(i))
             num += 1
             # print("正在提取第 {}/{} 个图片".format(num, currentPage))
-            pic = Picture(photo["created_at"], photo["pic_name"], photo["timestamp"],
+            pic = Picture(photo["photo_id"], photo["pic_name"], photo["timestamp"],
                           photo["pic_host"].replace("\\", ""))
             picList.append(pic)
         self.downloadPic(picList, currentPage)
