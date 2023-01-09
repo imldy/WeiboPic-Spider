af4ad934c1049e9a0e6c6758de07815ef04c0e7e
imldy <im.ldy@outlook.com>
2023-01-09 20:34:31 +0800

    增加请求头中的Referer，修复403问题

--- mian.py
+++ mian.py
@@ -56,6 +56,7 @@ class User(object):
         self.session = requests.session()
         self.session.headers = {
             "cookie": COOKIE,
+            "Referer": "https://weibo.com/",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
         }
         self.countNum = 0
