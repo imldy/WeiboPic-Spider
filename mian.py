05973059873e7e18d28f69243baa31c2f889366a
imldy <im.ldy@outlook.com>
2023-01-09 20:35:21 +0800

    注释

--- mian.py
+++ mian.py
@@ -59,6 +59,7 @@ class User(object):
             "Referer": "https://weibo.com/",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
         }
+        # 统计本用户一共下载了多少图片
         self.countNum = 0
 
     def getAllPic(self, blogger):
